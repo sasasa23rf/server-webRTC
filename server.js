3589505fb37ba80eed916e0e3d982027a0218461
sasasa23rf <salvo.co97@libero.it>
2025-11-21 16:30:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,27 +4,54 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-// Abilitiamo CORS per permettere al tuo file HTML locale di connettersi
+
 const io = new Server(server, {
     cors: {
-        origin: "*", // Accetta connessioni da ovunque (incluso il tuo file locale)
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
 
 const PORT = process.env.PORT || 3000;
 
+// --- CONFIGURAZIONE GLOBALE CONDIVISA ---
+// Questi sono i valori di default. Verranno sovrascritti se qualcuno li cambia dalla UI.
+let currentConfig = {
+    target_fps: 50,
+    width: 640,
+    height: 480,
+    jpeg_quality: 50
+};
+
 app.get('/', (req, res) => {
-    res.send('Server Ponte Video Attivo! In attesa di frame dal Raspberry...');
+    res.send('Server Ponte Video Attivo! Configurazione corrente: ' + JSON.stringify(currentConfig));
 });
 
 io.on('connection', (socket) => {
     console.log('Nuovo client connesso:', socket.id);
 
-    // Quando arriva un frame dal Raspberry (evento 'video_frame')
+    // 1. APPENA CONNESSO: Invia la configurazione attuale al nuovo client (sia esso chi guarda o il Raspberry)
+    socket.emit('config_updated', currentConfig);
+
+    // 2. RICEZIONE FRAME: Quando arriva un frame dal Raspberry
     socket.on('video_frame', (data) => {
-        // Lo inoltriamo immediatamente a tutti gli altri (il browser)
         socket.broadcast.emit('stream_display', data);
+    });
+
+    // 3. RICHIESTA CONFIG: Se un client chiede esplicitamente la config
+    socket.on('get_config', () => {
+        socket.emit('config_updated', currentConfig);
+    });
+
+    // 4. AGGIORNAMENTO CONFIG: Quando l'utente cambia la qualità dal sito
+    socket.on('update_config', (newConfig) => {
+        console.log('Nuova configurazione ricevuta:', newConfig);
+        
+        // Aggiorniamo la configurazione in memoria (unendo i vecchi valori con i nuovi)
+        currentConfig = { ...currentConfig, ...newConfig };
+
+        // Avvisiamo TUTTI (Soprattutto il Raspberry Pi) che la config è cambiata
+        io.emit('config_updated', currentConfig);
     });
 
     socket.on('disconnect', () => {
